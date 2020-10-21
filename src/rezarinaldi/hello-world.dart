b43772e735a82e911bbe3ec50a1990868e1b2bed
Reza Rinaldi <rez4rinaldi@gmail.com>
2020-10-21 15:06:27 +0700

    Adding hello-world.dart (#3)

---
+++ src/rezarinaldi/hello-world.dart
@@ -0,0 +1,8 @@
+void main() {
+    String nama = "Reza Rinaldi";
+    String namaEvent = "Hacktoberfest 2020";
+
+    print("Hello World!");
+    print("Hello an amazing event, $namaEvent");
+    print("Ttd, $nama");
+}
