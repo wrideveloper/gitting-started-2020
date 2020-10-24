791a7606705b06e4f003adbc6f371e0419a6ed1b
Ivan Abdurrafie <iabdurrafie48@gmail.com>
2020-10-24 11:45:18 +0700

    Add even odd number (#18)

---
+++ src/ivanabdurrafie/number.js
@@ -0,0 +1,9 @@
+var num=5;
+function even_or_odd(num) {
+    if(num % 2 == 0) {
+      return 'your number is Even';
+    } else {
+      return 'your number is Odd';
+    }
+};
+console.log(even_or_odd(num));
