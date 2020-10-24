9bed553df834e309448fe3f83b503e6f8d889ab8
Dharma Yudistira Eka Putra <40027558+dharmayudistira@users.noreply.github.com>
2020-10-24 13:42:41 +0700

    adding main.java (#46)

---
+++ src/dharmayudistira/Main.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dharma.operasimatematika;
+
+/**
+ *
+ * @author dharma
+ */
+public class Main {
+
+    public int addition(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+    public int substraction(int firstNumber, int secondNumber) {
+        return firstNumber - secondNumber;
+    }
+
+    public int divide(int firstNumber, int secondNumber) {
+        return firstNumber / secondNumber;
+    }
+    
+    public int square(int number) {
+        return number * number;
+    }
+}
